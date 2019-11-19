7f161320586ce019a4021f77bbcb35b9c5686c26
Luciferclc <luciferclc@foxmail.com>
2019-11-19 19:15:37 +0800

    init service

---
+++ controller/index.js
@@ -0,0 +1,14 @@
+
+// API分发至控制器
+
+exports.tag = require('./tag.controller')
+exports.heros = require('./hero.controller')
+exports.like = require('./like.controller')
+exports.auth = require('./auth.controller')
+exports.qiniu = require('./qiniu.controller')
+exports.option = require('./option.controller')
+exports.article = require('./article.controller')
+exports.comments = require('./comments.controller')
+exports.music = require('./music.controller')
+exports.book = require('./book.controller')
+exports.link = require('./link.controller')
